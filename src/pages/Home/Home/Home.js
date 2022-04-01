6db0403e550d47a24a62b9be5c3e2a63afae5614
farzanapomy <farzanapomy56@gmail.com>
2022-04-02 00:41:44 +0600

    updated

--- src/pages/Home/Home/Home.js
+++ src/pages/Home/Home/Home.js
@@ -1,6 +1,5 @@
 import React from 'react';
-import AnimationCar from '../../../components/Banner/AnimationCar/AnimationCar';
-import Banner from '../../../Components/Banner/Banner';
+// import AnimationCar from '../../../components/Banner/AnimationCar/AnimationCar';
 import Appbar from '../../Shared/Appbar/Appbar';
 import Facilities from '../Facilities/Facilities';
 
@@ -9,8 +8,8 @@ const Home = () => {
     return (
         <div>
             <Appbar></Appbar>
-            <Banner></Banner>
-            <AnimationCar></AnimationCar>
+            {/* <Banner></Banner> */}
+            {/* <AnimationCar></AnimationCar> */}
             <Facilities></Facilities>
         </div>
     );
