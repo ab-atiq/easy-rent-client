9c2281f7373b118b2cb455754d9f0ce36d9b25b5
farzanapomy <farzanapomy56@gmail.com>
2022-04-01 22:29:19 +0600

    farzana

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <span>
+        <span>  
           <span>Learn </span>
           <a
             className="App-link"
