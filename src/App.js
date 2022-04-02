edf33e49af44723e6a57eb64453ec07f6d977ee4
ATIQUR RAHMAN <abulbasharatiqur@gmail.com>
2022-04-02 10:18:19 +0600

    errro solve

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import './App.css';
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Login from './components/Login/Login';
-import Register from './components/Register/Register';
 import Footer from './components/Home/Footer/Footer';
 import Home from './pages/Home/Home/Home';
 
@@ -12,8 +10,6 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path='/' element={<Home />} />
-          <Route path='login' element={<Login />} />
-          <Route path='register' element={<Register />} />
         </Routes>
         <Footer></Footer>
       </BrowserRouter>
