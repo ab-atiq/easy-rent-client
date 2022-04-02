f602e15f21324105ce7bd3d0d4fccd284e48ebd3
ATIQUR RAHMAN <abulbasharatiqur@gmail.com>
2022-04-02 10:35:22 +0600

    some folder delete

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Login from './components/Login/Login';
-import Register from './components/Register/Register';
 import Footer from './components/Home/Footer/Footer';
 import Home from './pages/Home/Home/Home';
 
@@ -11,8 +9,6 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path='/' element={<Home />} />
-          <Route path='login' element={<Login />} />
-          <Route path='register' element={<Register />} />
         </Routes>
       </BrowserRouter>
     </div>
