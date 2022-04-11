f0504cd058848279084292f4c7ad0cab891d8479
ATIQUR RAHMAN <abulbasharatiqur@gmail.com>
2022-04-11 15:56:28 +0600

    question

--- src/pages/Home/Question/Accordion/Accordion6.js
+++ src/pages/Home/Question/Accordion/Accordion6.js
@@ -90,7 +90,7 @@ const CustomizedAccordions6 = () => {
                     <Typography>What should I look for when I'm choosing a car?</Typography>
                 </AccordionSummary>
                 <AccordionDetails>
-                    <Typography>
+                    <Typography sx={{marginLeft:"20px"}}>
                         <ul>
                             <li>Space: You'll enjoy your rental far more if you choose a car with plenty of room for your passengers and luggage.</li>
                             <li>Fuel policy: Not planning on driving much? A Like for like fuel policy can save you a lot of money.</li>
