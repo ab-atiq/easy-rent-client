df1e94f1e4a376eab9c1c5f12ce76b1f70e7d389
RonyMiah <ronymiah2121@gmail.com>
2022-04-11 23:04:51 +0600

    ss

--- src/Services/Service.js
+++ src/Services/Service.js
@@ -3,15 +3,19 @@ import * as React from "react";
 import { Link } from "react-router-dom";
 import "./Services.css";
 const Service = ({ service }) => {
-  const { _id, car_name, price, image , price_daily} = service;
+  const { _id, car_name, price, image, price_daily } = service;
   return (
     <div className="Singel_card">
-      <div >
+      <div>
         <img className="image_Optimize" src={image} alt="" />
         <h2>{car_name}</h2>
         <h4> Daily Rent $ ({price_daily}) </h4>
         <br />
-        <Link className="btn_design " to=""><Button className="btn_color" variant="contained">Book Now</Button></Link>
+        <Link className="btn_design " to="/">
+          <Button className="btn_color" variant="contained">
+            Book Now
+          </Button>
+        </Link>
       </div>
     </div>
   );
