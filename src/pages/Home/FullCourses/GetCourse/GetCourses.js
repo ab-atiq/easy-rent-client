4c6704f61a8c9a1980b04605e1fc8ff6e7d75cb8
farzanapomy <farzanapomy56@gmail.com>
2022-04-14 02:03:06 +0600

    sweet alert added

--- src/pages/Home/FullCourses/GetCourse/GetCourses.js
+++ src/pages/Home/FullCourses/GetCourse/GetCourses.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { Box } from '@mui/system';
-import { Container } from '@mui/material';
-
+import { Button, Container, Grid } from '@mui/material';
+import Swal from 'sweetalert2'
 // import './Services.css'
 
 const GetCourses = () => {
@@ -14,6 +14,14 @@ const GetCourses = () => {
     }, [])
 
 
+    const handleAlert = (id) => {
+        Swal.fire(
+            'Good job!',
+            'You successfully completed this course!',
+            'success'
+        )
+
+    }
 
     return (
         <>
@@ -21,27 +29,28 @@ const GetCourses = () => {
 
                 <div className='my-5 service-title'>
 
-                    <h1>Our Courses</h1>
+                    <h1>Become A Safer</h1>
 
                     <h6>Professional Photography Services. If you need photographs for your company website or a new printed sales brochure, our professional photographers will..</h6>
 
                 </div>
 
-                <Box xs={12} md={3} className="g-4 " >
+                <Grid container spacing={2} sx={{ my: "20px" }}>
 
                     {
-                        allCourses.map(allCourse => <Box
+                        allCourses.map(allCourse => <Grid xs={12} md={3}
                             key={allCourse._id}
                             service={allCourse}
                         >
 
-                            <iframe width="560" height="315" src={allCourse.youtube_video_link} title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                            <iframe width="300" height="200" src={allCourse.youtube_video_link} title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 
                             <h2>{allCourse.title}</h2>
                             <p>{allCourse.description}</p>
-                        </Box>)
+                            <Button onClick={handleAlert} variant='contained'>Complete Course</Button>
+                        </Grid>)
                     }
-                </Box>
+                </Grid>
 
             </Container >
         </>
