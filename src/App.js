b72d96597fcf4713e758f641222a855d961d71bb
farzanapomy <farzanapomy56@gmail.com>
2022-04-16 14:04:08 +0600

    google signin

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import Register from './pages/Shared/Register/Register';
 import NotFound from './pages/NotFound/Notfound';
 import AllServices from './pages/Home/Services/HomeServices/AllServices';
 import ContactUs from './pages/ContactUs/ContactUs';
-import './App.css';
 import Small from './pages/Home/Services/CarType/Small';
 import MainService from './pages/Home/Services/DetailsCar/MainService';
 import Medium from './pages/Home/Services/CarType/Medium';
@@ -14,30 +13,39 @@ import Appbar from './pages/Shared/Appbar/Appbar';
 import Dashbord from './pages/Dashbord/Dashbord';
 import CourseHome from './pages/Home/FullCourses/CourseHome/CourseHome';
 import GetCourses from './pages/Home/FullCourses/GetCourse/GetCourses';
+import './App.css';
+import { useSelector } from 'react-redux';
+import AuthProvider from './contexts/AuthProvider';
 
 
 function App() {
+
   return (
     <div className="App">
-      <BrowserRouter>
-        {/* <Appbar></Appbar> */}
-        <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/home' element={<Home />} />
-          <Route path='/contactUs' element={<ContactUs />} />
-          <Route path='/services' element={<AllServices />} />
-          <Route path='/details' element={<MainService />} />
-          <Route path='/small' element={<Small />} />
-          <Route path='/medium' element={<Medium />} />
-          <Route path='/large' element={<Large />} />
-          <Route path='/login' element={<Login />} />
-          <Route path='/register' element={<Register />} />
-          <Route path='/dashbord' element={<Dashbord />} />
-          <Route path='/CourseHome' element={<CourseHome />} />
-          <Route path='/GetCourses' element={<GetCourses />} />
-          <Route path='*' element={<NotFound />} />
-        </Routes>
-      </BrowserRouter>
+
+      <AuthProvider>
+
+        <BrowserRouter>
+          {/* <Appbar></Appbar> */}
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/home' element={<Home />} />
+            <Route path='/contactUs' element={<ContactUs />} />
+            <Route path='/services' element={<AllServices />} />
+            <Route path='/details' element={<MainService />} />
+            <Route path='/small' element={<Small />} />
+            <Route path='/medium' element={<Medium />} />
+            <Route path='/large' element={<Large />} />
+            <Route path='/login' element={<Login />} />
+            <Route path='/register' element={<Register />} />
+            <Route path='/dashbord' element={<Dashbord />} />
+            <Route path='/CourseHome' element={<CourseHome />} />
+            <Route path='/GetCourses' element={<GetCourses />} />
+            <Route path='*' element={<NotFound />} />
+          </Routes>
+        </BrowserRouter>
+      </AuthProvider>
+
     </div>
   );
 }
--- src/app/store.js
+++ src/app/store.js
@@ -1,10 +1,13 @@
 import { configureStore, combineReducers } from "@reduxjs/toolkit";
+import loginReducer from "./redux/loginReducer";
 import userReducer from "./redux/userReducer";
 
 
 export default configureStore({
   reducer: {
-    user: userReducer
+    regis: userReducer,
+    login: loginReducer,
+
   }
 });
 
