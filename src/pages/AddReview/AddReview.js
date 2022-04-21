a0cc3ef19805215808104dae46bec7eb0e0a8219
rakibemon <rakibemon03@gmail.com>
2022-04-22 01:33:51 +0600

    review section added

--- src/pages/AddReview/AddReview.js
+++ src/pages/AddReview/AddReview.js
@@ -1,28 +1,48 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import axios from 'axios';
+import Swal from 'sweetalert2';
 import { useForm } from "react-hook-form";
-import { Button, Grid, Typography, Input, FormHelperText, TextField } from '@mui/material';
-import { FormControlUnstyled } from '@mui/base';
+import { Grid, Typography, TextField } from '@mui/material';
 import InputLabel from '@mui/material/InputLabel';
-import { Label } from '@mui/icons-material';
 import MenuItem from '@mui/material/MenuItem';
 import FormControl from '@mui/material/FormControl';
 import Select from '@mui/material/Select';
 import useAuth from '../../hooks/useAuth';
+import { useNavigate } from 'react-router-dom';
 
 
 const AddReview = () => {
     const { user } = useAuth()
     const { register, handleSubmit, reset, formState: { errors }, } = useForm();
+    const navigate = useNavigate()
     const onSubmit = (data) => {
-        console.log(data)
-        reset();
+        data.name = user?.displayName;
+        data.imgUrl = user?.photoURL;
+        axios.post('https://guarded-taiga-13015.herokuapp.com/api/addreview', data)
+            .then(data => {
+                if (data?.statusText==='Created') {
+                    Swal.fire({
+                        position: 'center',
+                        icon: 'success',
+                        title: 'Your Review has been saved',
+                        showConfirmButton: false,
+                        timer: 1500
+                    })
+                    navigate(-1)
+                    reset();
+                }
+            })
     };
+    //change the title when change the route
+    useEffect(() => {
+        document.title = 'Add Review';
+    }, []);
     return (
         <div className='login-container'>
             <div className='login-grid'>
                 <form onSubmit={handleSubmit(onSubmit)}>
                     <Typography variant="h6" component="div" gutterBottom>
-                        Hi <span style={{color:'tomato'}}>{user?.displayName}</span> Your Opinion will help us to improve
+                        Hi <span style={{ color: 'tomato' }}>{user?.displayName}</span> Your Opinion will help us to improve
                     </Typography>
                     <Grid container spacing={3} maxWidth="lg" justifyContent="center"
                         alignItems="center">
@@ -32,18 +52,18 @@ const AddReview = () => {
                         <Grid item xs={12} md={6}>
                             <FormControl variant="filled" sx={{ m: 1, minWidth: 120 }}>
                                 <InputLabel id="demo-simple-select-filled-label">Rating</InputLabel>
-                                <Select>
-                                    <MenuItem value={1}>One</MenuItem>
-                                    <MenuItem value={2}>Two</MenuItem>
-                                    <MenuItem value={3}>Three</MenuItem>
-                                    <MenuItem value={4}>Four</MenuItem>
-                                    <MenuItem value={4.5}>Four and Half</MenuItem>
-                                    <MenuItem value={5}>Five</MenuItem>
+                                <Select {...register("rating")}>
+                                    <MenuItem value={1}>1</MenuItem>
+                                    <MenuItem value={2}>2</MenuItem>
+                                    <MenuItem value={3}>3</MenuItem>
+                                    <MenuItem value={4}>4</MenuItem>
+                                    <MenuItem value={4.5}>4.5</MenuItem>
+                                    <MenuItem value={5}>5</MenuItem>
                                 </Select>
                             </FormControl>
                         </Grid>
                         <Grid item md={12} sx={{ width: '100%' }}>
-                            <TextField fullWidth
+                            <TextField fullWidth {...register("opinion")}
                                 id="outlined-textarea"
                                 label="Your Opinion"
                                 placeholder="Your Opinion"
