04db2088f683537b62caac0d9e6e7ca1ff4b3421
strtanberahmed <tanber1111@gmail.com>
2022-05-07 20:54:25 +0300

    profile updated

--- src/pages/Home/porsche/Porsche.js
+++ src/pages/Home/porsche/Porsche.js
@@ -1,4 +1,3 @@
-import { Container } from '@mui/material'
 import Car from "../../../images/car.png";
 import './Porsche.css';
 import React from 'react';
@@ -6,20 +5,18 @@ import React from 'react';
 const Porsche = () => {
 
     return (
-        <Container>
-            <div className="main">
-                <div className="text">
-                    <h1>Long-Journey experience with
-                        <br /> short commute convenience.</h1>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi aliquid repellendus id illum sit quasi?
-                        <br /> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod harum vitae tempora quia quidem nostrum molestiae,
-                        <br /> dignissimos fuga qui exercitationem.</p>
-                </div>
-                <div className="car">
-                    <img src={Car} alt="" />
-                </div>
+        <div className="main">
+            <div className="text">
+                <h1>Long-Journey experience with
+                    <br /> short commute convenience.</h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi aliquid repellendus id illum sit quasi?
+                    <br /> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod harum vitae tempora quia quidem nostrum molestiae,
+                    <br /> dignissimos fuga qui exercitationem.</p>
             </div>
-        </Container>
+            <div className="car">
+                <img src={Car} alt="" />
+            </div>
+        </div>
     );
 };
 
