20ebaa6ea482ea66f0f4baec891de4df5a3b24ad
RonyMiah <ronymiah2121@gmail.com>
2022-05-16 21:30:54 +0600

    dele methode create done

--- src/pages/Home/Services/HomeServices/ManageOrder/ManageOrder.js
+++ src/pages/Home/Services/HomeServices/ManageOrder/ManageOrder.js
@@ -1,22 +1,19 @@
+
 import { Container, Grid } from "@mui/material";
 import React, { useEffect, useState } from "react";
 import useAuth from "../../../../../hooks/useAuth";
 import ManageOrderCart from "./ManageOrderCart";
-
+ 
 const ManageOrder = () => {
   const [services, setServices] = useState([]);
   const { user } = useAuth();
-
+ 
   useEffect(() => {
     fetch(`http://localhost:5000/api/orders/${user?.email}`)
       .then((res) => res.json())
       .then((data) => setServices(data));
-  }, [services]);
-
-  const ser = services?.serviceId;
-
-  console.log(ser);
-
+  }, [user?.email]);
+ 
   return (
     <div>
       <Container>
@@ -26,16 +23,24 @@ const ManageOrder = () => {
           spacing={{ xs: 2, md: 3, lg: 5 }}
           columns={{ xs: 12, sm: 12, md: 12, lg: 12 }}
         >
-          {services.map((cardata) => (
-            <ManageOrderCart
-              key={cardata._id}
-              alldata={cardata.serviceId}
-            ></ManageOrderCart>
-          ))}
+          {services.map((cardata) => {
+            const data = {
+              ...cardata.serviceId,
+              _id: cardata._id,
+            };
+            return (
+              <ManageOrderCart
+                key={cardata._id}
+                alldata={data}
+                services={services}
+                setServices={setServices}
+              ></ManageOrderCart>
+            );
+          })}
         </Grid>
       </Container>
     </div>
   );
 };
-
+ 
 export default ManageOrder;
