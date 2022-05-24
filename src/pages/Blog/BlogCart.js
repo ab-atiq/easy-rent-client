ba6ea3116a18a884d38312adf66a661025cd79c4
nurboss <nurmohammod993@gmail.com>
2022-05-24 16:41:38 +0600

    nur

--- src/pages/Blog/BlogCart.js
+++ src/pages/Blog/BlogCart.js
@@ -4,8 +4,10 @@ const BlogCart = ({alldata}) => {
   const {image, title, details, picture } = alldata;
   return (
     <div className="bolgcartbody">
+        <div className="blog-img">
         {!picture ? <img src={image} alt="" /> : 
         <img src={`data:img/png;base64,${picture}`} alt="" />}
+        </div>
 
         <h1>{title}</h1>
         <p>{details}</p>
